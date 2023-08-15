19989247da0be239816d35bc58bbb5f86e686d09
maikolfox <ahoueromeo@gmail.com>
2023-08-15 18:21:35 +0000

    ajout module details remise

---
+++ src/app/modules/admin/details-remise/details-remise.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetailsRemiseComponent } from './details-remise/details-remise.component';
+
+
+@NgModule({
+  declarations: [
+    DetailsRemiseComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DetailsRemiseModule { }
---
+++ src/app/modules/admin/details-remise/details-remise/details-remise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-remise',
+  templateUrl: './details-remise.component.html',
+  styleUrls: ['./details-remise.component.scss']
+})
+export class DetailsRemiseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
