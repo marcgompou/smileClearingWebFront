11f1025fb2462ec0eb1d3c112369e2822a166f3d
marcgompou <marcgompou>
2023-12-07 20:48:55 +0000

    parametrage prod et button

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true, 
     SOCKET_URL:"ws://localhost:996",
-    apiUrl:'https://10.225.203.33:7138/api',
+    apiUrl:'https://srvtbcolor01.bridgebankgroup.com:7138/api',
 };
